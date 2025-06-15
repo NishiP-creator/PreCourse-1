5dc96ec6f6fa55152b0d97302f52b7401335dce1
NishiP-creator <allp7821146@gmail.com>
2025-06-15 11:57:52 -0700

    final code Ex 1

--- Exercise_1.py
+++ Exercise_1.py
@@ -1,9 +1,27 @@
 class myStack:
   #Please read sample.java file before starting.
   #Kindly include Time and Space complexity at top of each file
+  """
+  In Python, len() does not iterate through a list. Python lists and collections store their length as a 
+  built-in attribute. So len() in
+  Lists --> O(1)
+  Sets --> O(1)
+  Dictionaries --> O(1)
+  Strings --> O(1)
+  So, isEmpty() and size() is O(1)
+
+  show() is O(n) because you are accessing each element in the stack one by one to display or return them.
+  """
 
   """
-  Time Complexity: O(n)
+  Time Complexity
+  isEmpty(): O(1)
+  push(): O(1)
+  pop(): O(1)
+  peek(): O(1)
+  size(): O(1)
+  show(): O(n)
+
   Space Complexity: O(n)
   """
 
@@ -12,26 +30,42 @@ class myStack:
       self.items = []
          
   def isEmpty(self):
-      return True if len(self.items) == 0 else False
+      return len(self.items) == 0
          
   def push(self, item):
       self.items.append(item)
          
   def pop(self):
       if self.isEmpty():
-          return -1
+          raise Exception("Stack is Empty")
       return self.items.pop()
         
   def peek(self):
       if self.isEmpty():
-          return -1
+          raise Exception("Stack is Empty")
       return self.items[-1]
         
   def size(self):
       return len(self.items)
          
   def show(self):
-      return self.items
+      """
+      return self.items violates encapsulation. Use __str__ (User-readable), __repr__ (Debugging; called by default when __str__ is missing, 
+      even for print()) or return a copy list(self.items).
+      Encapsulation means wrapping data and the methods that operate on that data into a single unit (a class), 
+      while hiding the internal details from the outside world. 
+      Encapsulation levels:
+      self.items	Public
+      self._items	Protected
+      self.__items	Private 
+      """
+      return str(self)
+
+  def __str__(self):
+      return "Stack displayed from top to bottom: " + " --> ".join(map(str, reversed(self.items)))
+  
+  def __repr__(self):
+      return "Stack displayed from top to bottom: " + " --> ".join(map(str, reversed(self.items)))
           
          
 
