5f86344715925bf9f879518427ae446e8090cdc8
NishiP-creator <allp7821146@gmail.com>
2025-06-15 12:51:12 -0700

    ex 2

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,3 +1,13 @@
+"""
+Time Complexity
+isEmpty(): O(1)
+push(): O(1)
+pop(): O(1)
+peek(): O(1)
+size(): O(1)
+show(): O(n)
+Space Complexity: O(n)
+"""
 
 class Node:
     def __init__(self, data):
@@ -6,27 +16,71 @@ class Node:
  
 class Stack:
     def __init__(self):
+        self.top = None
+        self.size = 0
         
+    def isEmpty(self):
+        return self.size == 0
+    
     def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.top # here you join new_node with the old_node i.e. next will be the old_node or None.
+        # You only have to keep track of the top node which is the new_node because its next will help you track 
+        # the items in the stack.
+        self.top = new_node
+        self.size += 1
         
     def pop(self):
+        if self.isEmpty():
+            raise IndexError("Stack is Empty")
+        item = self.top.data
+        self.top = self.top.next
+        self.size -= 1
+        return item
+    
+    def peek(self):
+        if self.isEmpty():
+            raise IndexError("Stack is Empty")
+        return self.top.data
+    
+    def size(self):
+        return self.size
+    
+    def show(self):
+        return str(self)
+    
+    def __str__(self):
+        current = self.top
+        items = []
+        while current:
+            items.append(current.data) # if item pushed is not a string, then you will get an error. Typecast
+                                       # into string --> str(current.data). Python expects each element in items 
+                                       # to be a string as join() can only join strings..
+            current = current.next
+        return "Stack displayed from top to bottom: " + " --> ".join(items)
         
 a_stack = Stack()
-while True:
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    print('push <value>')
-    print('pop')
-    print('quit')
-    do = input('What would you like to do? ').split()
-    #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
-    operation = do[0].strip().lower()
-    if operation == 'push':
-        a_stack.push(int(do[1]))
-    elif operation == 'pop':
-        popped = a_stack.pop()
-        if popped is None:
-            print('Stack is empty.')
-        else:
-            print('Popped value: ', int(popped))
-    elif operation == 'quit':
-        break
+a_stack.push('1')
+a_stack.push('2')
+a_stack.push('4')
+print(a_stack.pop())
+print(a_stack.show())
+
+# while True:
+#     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+#     print('push <value>')
+#     print('pop')
+#     print('quit')
+#     do = input('What would you like to do? ').split()
+#     #Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+#     operation = do[0].strip().lower()
+#     if operation == 'push':
+#         a_stack.push(int(do[1]))
+#     elif operation == 'pop':
+#         popped = a_stack.pop()
+#         if popped is None:
+#             print('Stack is empty.')
+#         else:
+#             print('Popped value: ', int(popped))
+#     elif operation == 'quit':
+#         break
